5335a9a434e14c3adae5daf1538545d6a228c876
MahiAlJawad <br.mahialjawad@gmail.com>
2023-01-01 17:20:50 +0600

    Initial Commit

---
+++ Instagram-layout-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram-layout-iOS
+//
+//  Created by Mahi Al Jawad on 1/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
